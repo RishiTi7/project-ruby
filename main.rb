c302904d999cefd939540006767b9ff586fea08e
rishikar <rishikar9849@gmail.com>
2023-04-24 18:10:55 +0530

    ruby notes

---
+++ main.rb
@@ -0,0 +1,27 @@
+#Basics
+
+print "hi rishi "
+print "welcome to ruby"  #print will just print out the line
+puts "Rishikar" # puts prints out on a new line
+puts "bhavan"
+
+#line by line execution top to bottom**
+puts"     /|"
+puts"    / |"
+puts"   /  |"
+puts"  /___|"
+
+
+# variables
+#string and ints in ""
+
+character_name = "Rishi"
+character_age = "22"
+
+puts ("there once was a man named " + character_name)
+puts ("he was " + character_age + " years old")
+character_name = "john" # we can update the variables in between
+puts ("he really liked the name "+character_name)
+puts ("but didnt like being " + character_age)
+
+# Data Types
