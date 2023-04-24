cec1f42f4ca0b6e4512a20db50b0b715dea25720
rishikar <rishikar9849@gmail.com>
2023-04-24 18:19:13 +0530

    commented out notes

--- main.rb
+++ main.rb
@@ -1,27 +1,28 @@
 #Basics
 
-print "hi rishi "
-print "welcome to ruby"  #print will just print out the line
-puts "Rishikar" # puts prints out on a new line
-puts "bhavan"
+# print "hi rishi "
+# print "welcome to ruby"  #print will just print out the line
+# puts "Rishikar" # puts prints out on a new line
+# puts "bhavan"
 
-#line by line execution top to bottom**
-puts"     /|"
-puts"    / |"
-puts"   /  |"
-puts"  /___|"
+# #line by line execution top to bottom**
+# puts"     /|"
+# puts"    / |"
+# puts"   /  |"
+# puts"  /___|"
 
 
-# variables
-#string and ints in ""
+# # variables
+# #string and ints in ""
 
-character_name = "Rishi"
-character_age = "22"
+# character_name = "Rishi"
+# character_age = "22"
 
-puts ("there once was a man named " + character_name)
-puts ("he was " + character_age + " years old")
-character_name = "john" # we can update the variables in between
-puts ("he really liked the name "+character_name)
-puts ("but didnt like being " + character_age)
+# puts ("there once was a man named " + character_name)
+# puts ("he was " + character_age + " years old")
+# character_name = "john" # we can update the variables in between
+# puts ("he really liked the name "+character_name)
+# puts ("but didnt like being " + character_age)
 
 # Data Types
+
